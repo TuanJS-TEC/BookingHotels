c9f20cc960f3dd55a35ed442eb61374c309647c3
TuanJS-TEC <codfish225@gmail.com>
2024-08-30 23:56:40 +0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const app = express();
+const connectDB = require("./config/db");
+
+const port = process.env.PORT || 6000;
+
+//connect to database
+connectDB();
+
+//setup middlewares
+app.use(express.json());
+
+app.listen(port,()=>console.log(`listening on on port ${port}`));
