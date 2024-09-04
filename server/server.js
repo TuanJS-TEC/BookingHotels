bfec6e3b20ed5b9d20e9cc2a9cf41867d75080ad
TuanJS-TEC <codfish225@gmail.com>
2024-09-04 20:19:12 +0700

    moi truong develop

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const connectDB = require("./config/db");
 
 const port = process.env.PORT || 6000;
+// const port = process.env.PORT || 5000; 
 
 //connect to database
 connectDB();
