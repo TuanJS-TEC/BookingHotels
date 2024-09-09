7f73e13c7bf973647d2ec5063f67bbca2db342f4
TuanJS-TEC <codfish225@gmail.com>
2024-09-09 07:23:50 +0700

    update code 9.9.2024

---
+++ server/routes/roomRoutes.js
@@ -0,0 +1,21 @@
+const { Router } = require ("express");
+
+const {
+    getRooms,
+    createRoom,
+    updateRooms,
+    deleteRoom,
+} = require("../controllers/roomController");
+// const { auth } = require("../middleware/authMiddletware");
+
+const router = Router();
+
+router.get("/",getRooms);
+
+// router.post("/",);
+
+router.get("/",(req,res)=>{
+    return res.json({mesage:"get room"});
+})
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,14 +2,18 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+const roomRoutes = require("./routes/roomRoutes");
+
 
 const port = process.env.PORT || 6000;
-// const port = process.env.PORT || 5000; 
 
 //connect to database
 connectDB();
 
 //setup middlewares
 app.use(express.json());
+
+//set up routes
+app.use("/api/rooms", roomRoutes);
 
 app.listen(port,()=>console.log(`listening on on port ${port}`));
